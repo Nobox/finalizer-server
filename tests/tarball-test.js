464aa493269851420c7edaeb67be4a8b99ff0eae
Jonathan Torres <jonathan@nobox.com>
2016-01-25 17:57:56 -0500

    Generate compression testing folder automatically.
    I had to create the folders manually in order to make the tests pass.

--- tests/tarball-test.js
+++ tests/tarball-test.js
@@ -1,46 +1,43 @@
 var Tarball = require('../lib/tarball');
 var expect = require('expect.js');
 var fs = require('fs');
-var rmdir = require( 'rmdir' );
+var rmdir = require('rmdir');
 
 describe('Tarball', function() {
 
-    describe('Compression Test', function(){
-        describe('compress() if source directory exists', function(){
-            // does the source dir exists ?
-            it('should compress without error', function (done){
-                var source = __dirname + '/files/compression-test/node_modules';
+    describe('Compression Test', function () {
+        describe('compress() if source directory exists', function () {
+            beforeEach(function () {
+                var path = __dirname + '/files/compression-test';
+                if (!fs.existsSync(path)) {
+                    fs.mkdirSync(path);
+                }
+            });
+
+            it('should compress without error', function (done) {
+                var source = __dirname + '/files/compression-test';
                 var destination = __dirname + '/files/compression-test/compressed-test.tar.gz';
-                Tarball.compress(source, destination, function(){
 
+                Tarball.compress(source, destination, function () {
                     var exists = fs.existsSync(source);
-
                     expect(exists).to.be(true);
-
                     fs.unlinkSync(destination);
-
                     done();
                 });
-
             });
         });
 
-        describe('extract() if compressed file is valid', function(){
-            it('should extract the file without errors', function(done){
-
+        describe('extract() if compressed file is valid', function () {
+            it('should extract the file without errors', function (done) {
                 var compressFile = __dirname + '/files/uncompression-test/temp.tar.gz';
-
                 var destination = __dirname + '/files/uncompression-test/';
 
-                Tarball.extract(compressFile, destination, function() {
+                Tarball.extract(compressFile, destination, function () {
                     var exists = fs.existsSync(destination + 'node_modules');
-
                     expect(exists).to.be(true);
-
-                    rmdir(destination + 'node_modules', function(err, dirs, files) {
+                    rmdir(destination + 'node_modules', function (err, dirs, files) {
                         done();
                     });
-
                 });
             });
         });
