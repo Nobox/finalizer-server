9c3033a98552171641a6ef45e3ce7b35a7b2d53f
Alberto Estrada Puerta <albertoe@gmail.com>
2016-01-25 16:45:08 -0500

    Created basic npm installer script

---
+++ tests/installler-test.js
@@ -0,0 +1,18 @@
+var Installer = require('../lib/installer');
+// var expect = require('expect.js');
+// var fs = require('fs');
+
+describe('Installer', function() {
+
+    describe('Installation Test', function(){
+        this.timeout(720000);
+        it('should install node modules inside a directory', function (done){
+            var directory = __dirname + '/files/installer-test/build/';
+
+            Installer.install(directory, function(){
+                done();
+            })
+        });
+    });
+
+});
