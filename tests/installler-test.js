ccde16672ebe599efec6ddc761ab8e2eda335b33
Alberto Estrada Puerta <albertoe@gmail.com>
2016-01-26 12:50:07 -0500

    Reduced install test timeout

--- tests/installler-test.js
+++ tests/installler-test.js
@@ -5,7 +5,7 @@ var Installer = require('../lib/installer');
 describe('Installer', function() {
 
     describe('Installation Test', function(){
-        this.timeout(720000);
+        this.timeout(30000);
         it('should install node modules inside a directory', function (done){
             var directory = __dirname + '/files/installer-test/build/';
 
