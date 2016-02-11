5f15cc0a4c1a91e58bca7f3aa04add25cbdea8a7
Jonathan Torres <jonathan@nobox.com>
2016-02-11 18:15:50 -0500

    Minor cs fixes.

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ var baseRoutes = require('./routes/base');
 var server = express();
 var PORT = 8080;
 
-server.get('/', function (req, res) {
+server.get('/', function(req, res) {
     res.send('Finalizer server.');
 });
 
 server.use('/api', baseRoutes);
 
-server.listen(PORT, function () {
+server.listen(PORT, function() {
     console.log('Finalizer server running on ' + PORT);
 });
