9da4d61325b71311188d4af0bbbe972e8a5cfc74
Jonathan Torres <jonathan@nobox.com>
2016-02-11 18:47:57 -0500

    Fix installer test.

--- tests/installer-test.js
+++ tests/installer-test.js
@@ -4,7 +4,7 @@ describe('Installer', function() {
     this.timeout(30000);
     it('should install node modules inside a directory', function(done) {
         var directory = __dirname + '/fixtures';
-        Installer.install(directory, function() {
+        Installer.install('npm', directory, function() {
             done();
         });
     });
