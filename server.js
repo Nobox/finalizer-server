ebad7a532519e3d0f1607a48dabafd21f80cfdf5
Jonathan Torres <jonathan@nobox.com>
2016-02-18 17:22:08 -0500

    Parse post requests with body parser.

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var baseRoutes = require('./routes/base');
 var server = express();
 var PORT = 8080;
+
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({ extended: true }));
 
 server.get('/', function(req, res) {
     res.send('Finalizer server.');
