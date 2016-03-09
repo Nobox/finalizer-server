7f378ce8f32874307c6a84204e96daed79fbc2b0
Jonathan Torres <jonathan@nobox.com>
2016-03-09 18:18:13 -0500

    Remove lines.

--- server.js
+++ server.js
@@ -4,15 +4,11 @@ var baseRoutes = require('./routes/base');
 var server = express();
 var habitat = require('habitat');
 var env = habitat.load('./.env');
-
-
-
 var PORT = process.env.SERVER_PORT;
 var HOST = process.env.PRIVATE_IP;
 
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({ extended: true }));
-
 
 server.get('/', function(req, res) {
     res.send('Finalizer server.');
