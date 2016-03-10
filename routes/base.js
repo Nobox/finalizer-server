820a33ae3304076d427477a336d9e32c43ca2a5f
Jonathan Torres <jonathan@nobox.com>
2016-03-10 16:01:22 -0500

    Add check endpoint.

--- routes/base.js
+++ routes/base.js
@@ -75,4 +75,12 @@ router.post('/download', function(req, res) {
     });
 });
 
+/**
+ * api/check
+ * Endpoint to check connection from CLI client.
+ */
+router.post('/check', function(req, res) {
+    res.json({ msg: req.body.status });
+});
+
 module.exports = router;
