9665da02f392011cfa6287fa2bf67eca1ae0b3dd
Alberto Estrada Puerta <albertoe@gmail.com>
2016-03-16 12:41:02 -0400

    Check if build was already downloaded

--- routes/base.js
+++ routes/base.js
@@ -64,7 +64,8 @@ router.post('/build', function(req, res) {
  */
 router.post('/download', function(req, res) {
     var projectName = req.body.name;
-    Finalizer.download(projectName, function(err, file) {
+    var dependencies = req.body.dependencies;
+    Finalizer.download(projectName, dependencies, function(err, file) {
         var response = { msg: '' };
         if (!err) {
             res.download(file);
